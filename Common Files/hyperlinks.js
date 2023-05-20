ecb0ce3926d0b77df9fddeec16d222502f0e4802
dinesh kumar <dinesh2468246@gmail.com>
2023-05-20 13:25:02 +0530

    Added new feature

---
+++ Common Files/hyperlinks.js
@@ -0,0 +1,7 @@
+ 
+document.querySelector('.fa-bag-shopping').addEventListener('click', function(){
+    window.location.href = "../wishlist/cart.html"
+})
+document.querySelector('.fa_cart').addEventListener('click', function(){
+    window.location.href = "../wishlist/cart.html"
+})
